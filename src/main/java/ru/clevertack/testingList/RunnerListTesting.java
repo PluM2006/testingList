e2528b5964fc783cd622308f2533a421abab9c4d
PluM2006 <PluM2006@tut.by>
2022-04-13 11:38:24 +0300

    init commit

---
+++ src/main/java/ru/clevertack/testingList/RunnerListTesting.java
@@ -0,0 +1,13 @@
+package ru.clevertack.testingList;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RunnerListTesting {
+
+	public static void main(String[] args) {
+	
+	}
+
+}
